8ce7e4fe8f983648dbbee4a4923de99b8eb0bd93
Genzer Hawker <genzers@gmail.com>
2019-05-20 15:17:16 +0700

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='bitbucklet',
     description = "A small CLI to manage BitBucket Cloud",
     author = "Genzer Hawker",
     author_email = "genzers@gmail.com",
-    version='0.1.0',
+    version='0.2.0',
     py_modules=['bitbucklet.cli'],
+    packages = find_packages(),
     install_requires=[
         'Click',
         'requests',
