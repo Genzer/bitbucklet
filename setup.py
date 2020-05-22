1e442564adff58e5ff234024ddad92560c1a056a
hau.tran <hau.tran@ubitec.ch>
2020-05-22 14:17:36 +0700

    Bump version 0.4.0-dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description = "A small CLI to manage BitBucket Cloud",
     author = "Genzer Hawker",
     author_email = "genzers@gmail.com",
-    version='0.3.1',
+    version='0.4.0-dev',
     py_modules=['bitbucklet.cli'],
     packages = find_packages(),
     install_requires=[
