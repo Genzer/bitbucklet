e38f2e46329f5ca8ad0aabacc1c2c66dda76b701
hau.tran <hau.tran@ubitec.ch>
2021-04-27 11:50:36 +0700

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description = "A small CLI to manage BitBucket Cloud",
     author = "Genzer Hawker",
     author_email = "genzers@gmail.com",
-    version='0.5.0-develop',
+    version='0.5.0',
     py_modules=['bitbucklet.cli'],
     packages = find_packages(),
     install_requires=[
